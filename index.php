113dbe00097cde38eb23f56f2f16ceda49caa7f2
MasterOfTheTiger <ted.jameson@pm.me>
2018-08-17 22:35:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <div id="wrapper">
         <a id="read" class="button"  href="bible.php?translation=net&ref=Hebrews+4">Read the Bible</a>
         <div id="about">
-            <p>Heb12 is a project started in March of 2018 to make Bible applications for different operating systems. The name Heb12 refers to Hebrews 4:12. </p>
+            <p>Heb12 is a project started in March of 2018 to make Bible apps. The name Heb12 refers to Hebrews 4:12. </p>
             <blockquote>For the word of God is living and active and sharper than any double-edged sword, piercing even to the point of dividing soul from spirit, and joints from marrow; it is able to judge the desires and thoughts of the heart.</blockquote>
             <p>Gods word is very powerful. That is why we are working on making it even more accessible to modern technology.</p>
             <h2>More Info</h2>
